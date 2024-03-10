f9a8b6156e09255012b0540f716e428025dcd356
Jorge Cisneros <cisneros.jorge.a@gmail.com>
2024-03-09 20:39:11 -0600

    created clear function to clear screen, prompt function to prompt user, and print articles function

--- NewsAPI.py
+++ NewsAPI.py
@@ -3,6 +3,8 @@ import subprocess
 import sys
 import webbrowser
 
+from os import system, name
+
 try:
     from newsapi import NewsApiClient
 except:
@@ -17,23 +19,43 @@ class Article:
 
 class NewsAPI:
     def __init__(self):
-        client = NewsApiClient(api_key = apiKey.ApiKey)
-        top = client.get_top_headlines(page_size=10,country='us') 
         self.Articles = []
 
-        for i, v in enumerate(top['articles']):
+    def _getArticles(self):
+        client = NewsApiClient(api_key = apiKey.ApiKey)
+        top = client.get_top_headlines(page_size=10,country='us') 
+
+        for v in top['articles']:
             a = Article()
             a.title = v['title']
             a.url = v['url']
             self.Articles.append(a)
-            print(f"\n{i+1}) {a.title}\n")
+
+    def _printArticles(self):
+        count = 1
+        print()
+        for i in self.Articles:
+            print(f"{count}) {i.title}\n")
+
+    def _clear(self):
+        system('cls') if name == 'nt' else system('clear')
+
+    def _prompt(self):
+        selection = 0
+
+        while selection != 'q':
+            selection = input("Print article (#) or (q)uit?: ")
+            if selection != 'q':
+                webbrowser.open(n.Articles[int(selection)-1].url)
+                self._clear()
+                self._printArticles()
+
+    def GetNews(self):
+        self._clear()
+        self._getArticles()
+        self._printArticles()
+        self._prompt()
 
 if __name__ == "__main__":
     n = NewsAPI()
-    
-    selection = 0
-
-    while selection != 'q':
-        selection = input("Print article (#) or (q)uit?: ")
-        if selection != 'q':
-            webbrowser.open(n.Articles[int(selection)-1].url)
+    n.GetNews()    
